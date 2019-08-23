3a945f9e0d2389c4dd7b96475986188794daea98
Juliana <julianagarca@gmail.com>
2019-08-23 11:29:28 -0300

    Testando energy harvesting

--- energySource.py
+++ energySource.py
@@ -5,27 +5,12 @@ Created on Wed Jan 30 09:42:49 2019
 @author: Juliana
 """
 import pandas as pd
-import numpy as np
 
 
-df = pd.read_csv('trace.csv', delimiter='\t')
-df.columns = ["a", "b", "c", "d", "e", "f", "g", "h", "i"]
+df = pd.read_csv('trace_24h.txt')
+a = 0
 
-print(df.shape)
-
-df = df[df.b != '     NaN']
-
-print(df.shape)
-
-#Mês
-#energia = df['b'][(df["c"]==2009) & (df["d"]==11)]
-
-#Dia
-energia = np.array(df['b'][(df["c"]==2009) & (df["d"]==11) & (df["e"]==15)])
-
-print(energia)
-
-
-with open('trace_24h.txt', 'w') as file:
-    for i in range(1, len(energia)):
-        file.write("{}\n".format(energia[i]))
+def harvest(bateria):
+    
+    bateria += df['energia'][a]
+    a += 1
