19ee4259ac19e322fe25c88764fd21a98aa65fed
Juliana <julianagarca@gmail.com>
2019-09-11 09:27:17 -0300

    Commit before pull

--- DC-LEACH.py
+++ DC-LEACH.py
@@ -4,11 +4,12 @@ import math
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from energySource import harvest
-from utils import gerarCenario, gastoRx, gastoTx, ajuste_alcance_nodeCH, checaBateria, contEncaminhamento, desvio_padrao, distancia, localizaObjetoCH, maiorLista, menorLista, setorizacao, setorizacaoCH, verifica_eleitos
+import time
+import config as cf
+from energySource import irradiacao, harvest
+from utils import gerarCenario, gastoRx, gastoTx, ajuste_alcance_nodeCH, checaBateria, contEncaminhamento, desvio_padrao, distancia, localizaObjetoCH, setorizacao, setorizacaoCH, verifica_eleitos
 
 
-############################### Functions ################################
 def selecao_CH(nodes, Round, Porcentagem):
   CH = []
   for k in nodes:
@@ -21,61 +22,37 @@ def selecao_CH(nodes, Round, Porcentagem):
   return CH
 
 
-
 ############################### Variables ################################
 CH = []
-tamPacoteConfig = 300
 
-modosHop = [[0,0],[0,1],[1,0],[1,1]]
-#modosHop = [[0,0]]
-
-list_qtdNodes = [100]
-list_qtdFrames = [10]
-list_tamPacoteTransmissao = [2000]
-list_percentualCH = [0.05]
-list_qtdSetores = [4]
-list_area = [100]
-
-total_simulacoes = 33
 arquivo = open('novo-arquivo.txt', 'w')
-arquivo_bat = open('bateria.txt', 'w')
+arquivo_bat = open('bateria.txt', 'a')
 
 ############################### Main ################################
 # Realiza a variação de um dos cenários (Quem usar a variável: cenario)
 
-qtdNodes =                         list_qtdNodes[0]
-qtdFrames =                       list_qtdFrames[0]
-tamPacoteTransmissao = list_tamPacoteTransmissao[0]
-percentualCH =                 list_percentualCH[0]
-qtdSetores =                     list_qtdSetores[0]
-area =                                 list_area[0]
+distMax = distancia(0,0, cf.area,cf.area)
 
-distMax = distancia(0,0, area,area)
-BS = [0, area+25.0, area/2, 0.0, 0]
-
-print("\n\nCENÁRIO: " + str(qtdNodes) + ' nodes, '
-                  + str(qtdFrames) + ' frames, '
-                  + str(tamPacoteTransmissao) + ' bits, '
-                  + str(int(percentualCH*100)) + '%, '
-                  + str(int(qtdSetores)) + ' setores, '
-                  + str(int(area)) + ' m2')
+print("\n\nCENÁRIO: " + str(cf.qtdNodes) + ' nodes, '
+                  + str(cf.qtdFrames) + ' frames, '
+                  + str(cf.tamPacoteTransmissao) + ' bits, '
+                  + str(int(cf.percentualCH*100)) + '%, '
+                  + str(int(cf.qtdSetores)) + ' setores, '
+                  + str(int(cf.area)) + ' m2')
 
 # Altera entre os modos de operação do multi-hop
-for modoOp in modosHop:
+for modoOp in cf.modosHop:
     intraCluster = modoOp[0]
     interCluster = modoOp[1]
-
-    modo = '>>>>>>>>>>>>>>>>>>>> Intracluster ' + str(intraCluster) + ' || Intercluster ' + str(interCluster) + ' <<<<<<<<<<<<<<<<<<<<<\n'
-    arquivo.write(modo)
 
     framesSimulacao = []
     roundsSimulacao = []
 
     # Iteração para realizar várias iterações (total de simulações)
-    for simulacao in range(total_simulacoes):
+    for simulacao in range(cf.total_simulacoes):
         Round = 1
         totalRounds = 0
-        nodes = gerarCenario(qtdNodes,distMax)
+        nodes = gerarCenario(cf.qtdNodes,distMax)
         nosVivos = list()
 
         if(interCluster == 1):
@@ -83,25 +60,27 @@ for modoOp in modosHop:
             # Trasmite as localizações à BS
             distanciasBS = []
             for k in nodes:
-                dist = distancia(k[2],k[3],BS[1],BS[2])
+                dist = distancia(k[2],k[3],cf.BS[1],cf.BS[2])
                 distanciasBS.append( dist )
-                k[1] = gastoTx(k[1],dist,tamPacoteConfig)
+                k[1] = gastoTx(k[1],dist,cf.tamPacoteConfig)
             # Recebe os setores da BS
             for k in nodes:
-                k[9] = setorizacaoCH(distanciasBS,distanciasBS[k[0]-1],qtdSetores)
-                k[1] = gastoRx(k[1],tamPacoteConfig)
+                k[9] = setorizacaoCH(distanciasBS,distanciasBS[k[0]-1],cf.qtdSetores)
+                k[1] = gastoRx(k[1],cf.tamPacoteConfig)
 
 
         # INICIO DA EXECUÇÃO DA SIMULAÇÃO
-        while(Round <= 5000 and len(nodes) != 0):
-
-            #arquivo_bat.write('>>>>>> Round ' + str(Round) + ' <<<<<<<\n')
+        while(Round <= 4000 and len(nodes) != 0):
 
             # Energy Harvesting
+            start = time.time()
+            H = irradiacao(Round)
             for n in nodes:
-                harvest(n[1], Round)
-                bateria = 'Nó ' + str(n[0]) + ': ' + str(n[1]) + '\n'
-                #arquivo_bat.write(bateria)
+                n[1] += harvest(H)
+                if n[1] >= 5.0:
+                    n[1] = 5.0
+            end = time.time()
+            print(end-start)
 
             #Verifica Reset do Round Superior
             if(verifica_eleitos(nodes)):
@@ -109,7 +88,7 @@ for modoOp in modosHop:
                     k[6] = 0
 
             # Realiza seleção de CH
-            CH = selecao_CH(nodes, Round, percentualCH)
+            CH = selecao_CH(nodes, Round, cf.percentualCH)
 
             # Conta os frames que foram executados
             totalFramesExecutados = 0
@@ -122,15 +101,15 @@ for modoOp in modosHop:
                 for k in CH:
                     pacotesBroadcast.append( [k[0],k[2],k[3],0.0,k[9]] )
                     # Registro da BS para envio
-                    k[7].append(BS)
-                    k[1] = gastoTx(k[1],k[4],tamPacoteConfig)
+                    k[7].append(cf.BS)
+                    k[1] = gastoTx(k[1],k[4],cf.tamPacoteConfig)
 
                 # RECEPÇÃO     CH: Chs recebem o broadcast dos outros CHs (os rádios estão ligados)
                 for k in CH:
                     for node in pacotesBroadcast:
                         if(node[0] != k[0]):
                             k[7].append(node)
-                    k[1] = gastoRx(k[1],tamPacoteConfig)
+                    k[1] = gastoRx(k[1],cf.tamPacoteConfig)
 
                 if(nodes != []):
                     # RECEPÇÃO    NCH: Recepção dos Pacotes de Bradcast
@@ -146,7 +125,7 @@ for modoOp in modosHop:
                         # Atualização dos valores
                         k[7] = [ nodeMenorDistancia ]
                         k[4] = menorDistancia
-                        k[1] = gastoRx(k[1],tamPacoteConfig)
+                        k[1] = gastoRx(k[1],cf.tamPacoteConfig)
 
                     # TRANSMISSÃO NCH: Envio de Pacotes Resposta
                     for k in nodes:
@@ -155,20 +134,20 @@ for modoOp in modosHop:
                         for nodeCH in CH:
                             if(k[7][0][0] == nodeCH[0]):
                                 nodeCH[8].append(node)
-                        k[1] = gastoTx(k[1],k[4],tamPacoteConfig)
+                        k[1] = gastoTx(k[1],k[4],cf.tamPacoteConfig)
 
                     # RECEPÇÃO     CH: Recepção de Pacotes de Resposta
                     for k in CH:
                         # Nodes atribuídos na função anterior
                         for l in range( len(k[8]) ):
-                            k[1] = gastoRx(k[1],tamPacoteConfig)
+                            k[1] = gastoRx(k[1],cf.tamPacoteConfig)
 
                     # TRANSMISSÃO  CH: Envio da Tabela TDMA
                     ajuste_alcance_nodeCH(CH)
                     clusters = []
                     for k in CH:
-                        clusters.append( [k[0], setorizacao(k[8],qtdSetores)] )
-                        k[1] = gastoTx(k[1],k[4],tamPacoteConfig)
+                        clusters.append( [k[0], setorizacao(k[8],cf.qtdSetores)] )
+                        k[1] = gastoTx(k[1],k[4],cf.tamPacoteConfig)
 
                     # RECEPÇÃO    NCH: Recepção da Tabela TDMA
                     for k in nodes:
@@ -177,11 +156,11 @@ for modoOp in modosHop:
                         for clstr in clusters:
                             if(clstr[0] == idCH):
                                 k[8] = clstr[1]
-                        k[1] = gastoRx(k[1],tamPacoteConfig)
+                        k[1] = gastoRx(k[1],cf.tamPacoteConfig)
 
                     # CONFIGURAÇÃO DE RADIO DOS CH PARA ALCANÇAR A BS
                     for k in CH:
-                        k[4] = distancia(k[2],k[3], BS[1],BS[2])
+                        k[4] = distancia(k[2],k[3], cf.BS[1],cf.BS[2])
 
                     # MULTI-HOP INTRACLUSTER
                     if(intraCluster == 1):
@@ -220,7 +199,7 @@ for modoOp in modosHop:
                         mapaEncaminhamento.append( k[7][0][0] )
 
                     # FRAMES
-                    for contFrame in range(qtdFrames):
+                    for contFrame in range(cf.qtdFrames):
                         confirmaFrame = 0
                         # NCH: Transmite Pacote
                         for k in nodes:
@@ -228,32 +207,32 @@ for modoOp in modosHop:
                                 # Gasto de agregação de dados
                                 totalContEnc = contEncaminhamento(k[0], mapaEncaminhamento)
                                 if(totalContEnc > 0):
-                                    k[1] = k[1] - (0.000000005*tamPacoteTransmissao*(totalContEnc + 1))
-                            k[1] = gastoTx(k[1],k[4],tamPacoteTransmissao)
+                                    k[1] = k[1] - (0.000000005*cf.tamPacoteTransmissao*(totalContEnc + 1))
+                            k[1] = gastoTx(k[1],k[4],cf.tamPacoteTransmissao)
                         # CH: Recebe Pacote
                         for k in CH:
                             for l in range( contEncaminhamento(k[0], mapaEncaminhamento) ):
-                                k[1] = gastoRx(k[1],tamPacoteTransmissao)
+                                k[1] = gastoRx(k[1],cf.tamPacoteTransmissao)
                         # NCH: Recebe Pacote
                         if(intraCluster == 1):
                             for k in nodes:
                                 for l in range( contEncaminhamento(k[0], mapaEncaminhamento) ):
-                                    k[1] = gastoRx(k[1],tamPacoteTransmissao)
+                                    k[1] = gastoRx(k[1],cf.tamPacoteTransmissao)
                         # CH: Envia Pacote para a BS
                         for k in CH:
                             # Gasto de agregação de dados
                             totalContEnc = contEncaminhamento(k[0], mapaEncaminhamento)
                             if(totalContEnc > 0):
-                                k[1] = k[1] - (0.000000005*tamPacoteTransmissao*(totalContEnc + 1))
+                                k[1] = k[1] - (0.000000005*cf.tamPacoteTransmissao*(totalContEnc + 1))
                             node = k
                             idDestino = node[7][0][0]
                             while(idDestino != 0):
-                                node[1] = gastoTx(node[1],node[4],tamPacoteTransmissao)
+                                node[1] = gastoTx(node[1],node[4],cf.tamPacoteTransmissao)
                                 node = localizaObjetoCH(idDestino,CH)
                                 # Gasto Recepção do node destino
-                                node[1] = gastoRx(node[1],tamPacoteTransmissao)
+                                node[1] = gastoRx(node[1],cf.tamPacoteTransmissao)
                                 idDestino = node[7][0][0]
-                            node[1] = gastoTx(node[1],node[4],tamPacoteTransmissao)
+                            node[1] = gastoTx(node[1],node[4],cf.tamPacoteTransmissao)
                             if(node[1] >= 0):
                                 # Confirma que houve um envio a BS
                                 confirmaFrame += 1
@@ -264,43 +243,49 @@ for modoOp in modosHop:
 
                 # FECHAMENTO DO ROUND
                 # Encerramento do Round
+                bat = list()
                 for k in CH:
                     nodes.append(k)
                 for k in nodes:
                     k[4] = distMax
                     k[7] = []
                     k[8] = []
-
+                    bat.append({k[0]: k[1]})
+                
                 #Exclui zerados
                 checaBateria(nodes)
 
                 nosVivos.append(len(nodes))
+                resultados = 'Round: ' + str(Round) + ' #Nós Vivos: ' + str(len(nodes)) + '\n'
+                #print('Simulação: ' + str(simulacao) + ' Round: ' + str(Round) + ' #Nós Vivos: ' + str(len(nodes)) + '\n')
+                #arquivo.write(resultados)
 
                 CH = []
                 Round = Round + 1
 
 
                 # FIM DE UM ROUND ##########
-                
+        arquivo_bat.write(str(Round) + str(bat) + "\n")
         df = pd.DataFrame(nosVivos, columns=['NosVivos'])
-        #print('Simulacao ' + str(simulacao+1) + ": " + str(totalFrames))
-        roundsSimulacao.append(Round)
+        print('Simulacao ' + str(simulacao+1) + ": " + str(Round))
+        roundsSimulacao.append(Round-1)
 
         resultados = 'Simulacao: ' + str(simulacao) + ' Rounds: ' + str(Round-1) + ' Nos Vivos: ' + str(len(nodes)) + '\n'
-        arquivo.write(resultados)
+        #arquivo.write(resultados)
 
         # FIM DE UMA SIMULAÇÃO ##########
     ############################### Estatísticas ################################
-    media = sum(roundsSimulacao) / total_simulacoes
+    media = sum(roundsSimulacao) / cf.total_simulacoes
 
     print('\nResultado do ' + str(modoOp[0]) + str(modoOp[1]) +"-LEACH-HOP:")
     print('Rounds: ', roundsSimulacao)
     print('Média: ' + str(media))
-    print('Erro: ' + str(1.96*(desvio_padrao(roundsSimulacao, media) / math.sqrt(total_simulacoes) )))
-
+    print('Erro: ' + str(1.96*(desvio_padrao(roundsSimulacao, media) / math.sqrt(cf.total_simulacoes) )))
+    arquivo.write("\nResultado do " + str(modoOp[0]) + str(modoOp[1]) +"-LEACH-HOP:\nRounds: "+ str(roundsSimulacao) +"\nMédia: " + str(media) +'\nErro: ' + str(1.96*(desvio_padrao(roundsSimulacao, media) / math.sqrt(cf.total_simulacoes) )))
     # FIM DE TODOS OS EXPERIMENTOS DE UM MODO DE OPERAÇÃO ##########
 
 #plt.plot(df.index, df['NosVivos'])
 #print("Last round:", max(df.index))
 
-
+arquivo.close()
+arquivo_bat.close()
--- config.py
+++ config.py
@@ -2,10 +2,14 @@
 '''
 Definição das configurações dos nós, rede e simulação
 '''
+totalSimulacoes = 33
 
 tamPacoteConfig = 300
 
-modosHop = [[0,0], [0,1], [1,0], [1,1]]
+bat_init = 2.5
+
+modosHop = [[0,0]]
+#modosHop = [[0,0], [0,1], [1,0], [1,1]]
 
 list_qtdNodes = [100]
 list_qtdFrames = [10]
@@ -24,8 +28,27 @@ percentualCH =                 list_percentualCH[0]
 qtdSetores =                     list_qtdSetores[0]
 area =                                 list_area[0]
 
+BS = [0, area+25.0, area/2, 0.0, 0]
+
 # [gorlatova2012networking]
 cell_size = 10      # cm^2
 cell_efic = 0.01
 round_len = 20      # segundos
 max_bat = 5.0       # Joules
+
+# DC-LEACH
+kmin = 5
+kmax = 20
+kopt = 0
+Ts = 0.1    # tempo de slot (segundo)
+Lr = 500    # tamanho do round (timeslots)
+Tr = Lr * Ts    # tempo do round (segundos)
+Lhor = 10   # horizonte de predição (rounds)
+
+cz = 0.000154   # m^2
+ez = 0.22
+Io = 200    # W/m^2
+
+Pmax_tx = 0.0801    # W
+Prx = 0.0222    # W
+p = 0.5     # pacotes/s
